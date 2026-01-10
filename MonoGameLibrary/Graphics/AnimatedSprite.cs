cbbd7537a06682056017565dc777aefe956cfb55
Natasha Robarge <robarge.natasha7@gmail.com>
2026-01-10 12:23:36 -0600

    create animated sprite class, extend sprite class...
    Update atlas def, use animated class for sprites

---
+++ MonoGameLibrary/Graphics/AnimatedSprite.cs
@@ -0,0 +1,70 @@
+using System;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameLibrary.Graphics;
+
+// The AnimatedSprite class extends the Sprite class to add animation capabilities,
+// using the concept of inheritance in OOP. (Similar to extending types in TypeScript)
+public class AnimatedSprite : Sprite
+{
+  // ----- Private Members ----- //
+
+  private int _currentFrame;
+  private TimeSpan _elapsed;
+  private Animation _animation;
+
+  // ----- Properties ----- //
+
+  /// <summary>
+  /// Gets or Sets the animation for this animated sprite.
+  /// </summary>
+  public Animation Animation
+  {
+    get => _animation;
+    set
+    {
+      _animation = value;
+      Region = _animation.Frames[0];
+    }
+  }
+
+  // ----- Constructors ----- //
+  /// <summary>
+  /// Creates a new animated sprite.
+  /// </summary>
+  public AnimatedSprite() { }
+
+  /// <summary>
+  /// Creates a new animated sprite with the specified frames and delay.
+  /// </summary>
+  /// <param name="animation">The animation for this animated sprite.</param>
+  public AnimatedSprite(Animation animation)
+  {
+    Animation = animation;
+  }
+
+  // ----- Methods ----- //
+
+  /// <summary>
+  /// Updates this animated sprite.
+  /// </summary>
+  /// <param name="gameTime">A snapshot of the game timing values provided by the framework.</param>
+  public void Update(GameTime gameTime)
+  {
+    _elapsed += gameTime.ElapsedGameTime;
+
+    if (_elapsed >= _animation.Delay)
+    {
+      _elapsed -= _animation.Delay;
+      _currentFrame++;
+
+      if (_currentFrame >= _animation.Frames.Count)
+      {
+        _currentFrame = 0;
+      }
+
+      Region = _animation.Frames[_currentFrame];
+    }
+  }
+
+}
