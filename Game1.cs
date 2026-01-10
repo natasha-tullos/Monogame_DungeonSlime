9920b0a3a4ec5ca537d6345bf0577ab91e1de5fb
Natasha Robarge <robarge.natasha7@gmail.com>
2026-01-10 09:37:02 -0600

    use pre-release version to fix mac OS bug w/content manager...
    add first asset to the content manager, following 2D tutorial

--- Game1.cs
+++ Game1.cs
@@ -7,6 +7,8 @@ namespace DungeonSlime;
 
 public class Game1 : Core
 {
+    private Texture2D _logo;
+
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
 
@@ -22,6 +24,9 @@ public class Game1 : Core
     protected override void LoadContent()
     {
         // TODO: use this.Content to load your game content here
+
+        // Load the image from the content pipeline
+        _logo = Content.Load<Texture2D>("images/logo");
 
         base.LoadContent();
     }
@@ -40,7 +45,14 @@ public class Game1 : Core
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin();
+
+        // Draw the logo texture
+        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
 
         base.Draw(gameTime);
     }
