91812610de57fdfa0a2d1af3320d67bc47a5f193
Natasha Robarge <robarge.natasha7@gmail.com>
2026-01-10 10:21:29 -0600

    Learned a bit more about sprite batch, textures, and added notes

--- Game1.cs
+++ Game1.cs
@@ -49,7 +49,22 @@ public class Game1 : Core
         SpriteBatch.Begin();
 
         // Draw the logo texture
-        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+        SpriteBatch.Draw(
+            _logo,              // texture
+            new Vector2(        // position
+                // I want to make an extra note here to remember that
+                // multiplying instead of dividing is an optimization technique
+                // that improves CPU performance. 
+                Window.ClientBounds.Width,
+                Window.ClientBounds.Height) * .5f,
+            null,               // sourceRectangle, null renders the entire texture
+            Color.White,        // color, renders the texture with no tint
+            0.0f,               // rotation, the amount of rotation in radians
+            new Vector2(_logo.Width, _logo.Height) * .5f,       // origin, x and y coordinates for position of texture origin
+            1.5f,               // scale, how to scale the texture, 1.0f is no scaling, normal size
+            SpriteEffects.None, // effects, should the texture be flipped horizontally, vertically, or both
+            0.0f                // layerDepth, the depth for which the texture is rendered, only applies with "SpriteSortMode.FrontToBack" or "SpriteSortMode.BackToFront"
+        );
 
         // Always end the sprite batch when finished.
         SpriteBatch.End();
